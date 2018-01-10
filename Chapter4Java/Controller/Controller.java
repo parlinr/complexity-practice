2a11092589348b2b180fd1b583109d3c579c5374
Ryan Parlin <parlinr@mail.nmc.edu>
2018-01-10 18:23:17 -0500

    Begin work on Java port of Chapter 4 sim, makefile created

---
+++ Chapter4Java/Controller/Controller.java
@@ -0,0 +1,6 @@
+
+public class Controller {
+	public Controller() {
+        System.out.println("edfgras");
+	}
+}
---
+++ Chapter4Java/README.rd
@@ -0,0 +1,1 @@
+This is a Java port of the Chapter 4 project in the ComplexityPractice (C#) subfolder
